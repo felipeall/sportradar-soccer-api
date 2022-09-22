a2306f1e61f50c28e557b3dea87a456a185066c8
Felipe Allegretti <felipeallh@gmail.com>
2022-09-22 14:08:43 -0300

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sportradar_soccer_api',
-    version='0.0.1',
+    version='0.0.2',
     packages=["sportradar_soccer_api"],
     description='',
     author='Felipe Allegretti',
